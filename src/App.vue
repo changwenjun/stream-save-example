7d6197525eee89701b4eeabbbb2e497bd89e6372
changwenjun <1573696153@qq.com>
2022-08-08 13:18:23 +0800

    删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <script setup lang="ts">
-import LocalFileDownload from "./components/LocalFileDownload.vue";
 import LocalFileZip from "./components/LocalFileZip.vue";
-import JSZip from "./components/JSZip.vue";
-import Dzip from "./components/dzip.vue";
 </script>
 
 <template>
-<!--  <Dzip />-->
-  <JSZip />
-  <LocalFileDownload />
   <LocalFileZip />
 </template>
 
